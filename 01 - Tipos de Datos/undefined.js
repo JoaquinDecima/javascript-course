516ad3140e35e5db53aaac984e1c96c9a6a76fcc
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-04-18 15:37:31 -0300

    add undefinde

---
+++ 01 - Tipos de Datos/undefined.js
@@ -0,0 +1,15 @@
+let x;
+
+console.log(x);
+
+if (x === undefined) {
+	console.log('x is undefined \n');
+} else {
+	console.log('x has been defined \n');
+}
+
+if (typeof y === 'undefined') {
+	console.log('y is undefined \n');
+} else {
+	console.log('y has been defined \n');
+}
