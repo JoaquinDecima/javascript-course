260cc9f235aa027ff920ff24cfa31400757b5545
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-04-18 10:09:25 -0300

    Add types example

---
+++ 01 - Tipos de Datos/types.js
@@ -0,0 +1,31 @@
+// Tipos primiticvos
+
+console.log(undefined);
+console.log(true); // Boolean
+console.log(false); // Boolean
+console.log(1);
+console.log(null);
+console.log(2n);
+console.log('Hola soy un string \n');
+
+// Viendo los tipos
+
+console.log(typeof undefined);
+console.log(typeof true); // Boolean
+console.log(typeof false); // Boolean
+console.log(typeof 1);
+console.log(typeof null);
+console.log(typeof 2n);
+console.log(typeof 'Hola soy un string\n');
+
+// Otros tipos
+
+console.log(() => {});
+console.log([]);
+console.log({});
+
+console.log('\n');
+
+console.log(typeof (() => {}));
+console.log(typeof []);
+console.log(typeof {});
